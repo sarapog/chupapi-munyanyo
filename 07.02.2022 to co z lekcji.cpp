98f76fe647900e225307b7b0fbb23dc0ff2fc0d2
sarapog <97165482+sarapog@users.noreply.github.com>
2022-03-04 16:28:27 +0100

    Add files via upload

---
+++ 07.02.2022 to co z lekcji.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+class komputer
+{
+public:
+    string pamiec;
+    string procesor;
+    string dysk;
+    string grafika;
+    string naped;
+
+    komputer(string pam = "DDR", string proc = "i7", string d = "2tb", string graf = "ATI", string n = "DVD")
+    {
+        cout << "Inicjacja konstruktora" << endl;
+        pamiec = pam;
+        procesor = proc;
+        dysk = d;
+        grafika = graf;
+        naped = n;
+    }
+    void wczytaj()
+    {
+        cout << "Podaj nazwe pamieci: " << endl;
+        cin >> pamiec;
+        cout << "Podaj nazwe procesora: " << endl;
+        cin >> procesor;
+        cout << "Podaj nazwe dysku: " << endl;
+        cin >> dysk;
+        cout << "Podaj nazwe grafika: " << endl;
+        cin >> grafika;
+        cout << "Podaj nazwe napędu: " << endl;
+        cin >> naped;
+
+    }
+    void wypisz()
+    {
+        cout << "wczytana pamiec to " << pamiec << endl;
+        cout << "wczytany procesor to " << procesor << endl;
+        cout << "wczytany dysk to " << dysk << endl;
+        cout << "wczytana grafika to " << grafika << endl;
+        cout << "wczytany napęd to " << naped << endl;
+    }
+    ~komputer()
+    {
+        cout << "destruktor czysci" << endl;
+    }
+ 
+};
+
+int main()
+{
+    komputer k2("DDR", "i7", "2tb", "ATI", "DVD");
+    k2.wypisz();
+}
+
+
